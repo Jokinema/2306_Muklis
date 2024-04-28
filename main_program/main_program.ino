9a64aa561a87fc66c3885b8b18c0b2a34898de9b
Dwi Setia Fardhana <84776462+CallmeSetia@users.noreply.github.com>
2024-04-28 17:47:08 +0700

    Update main_program.ino

--- main_program/main_program.ino
+++ main_program/main_program.ino
@@ -1,60 +1,75 @@
+#include "Kinematrix.h"
+#include "index/digital_input.h"
+#include "index/digital_output.h"
+#include "modules/communication/wireless/lora/lora-com.h"
+#include "modules/time/timer-task.h"
 
-void sendData() {
-  data = getDataProx();
-  if (data == 1 && THIS_DEVICE_ADDRESS != DEVICE_ADDRESS_RX) {
+#include <LiquidCrystal_I2C.h>
+
+
+
+#define DEBUG 1
+// #define SEND_RANDOM 0
+
+enum DeviceAddress {
+  DEVICE_ADDRESS_TX_1 = 0x01,
+  DEVICE_ADDRESS_RX = 0x02,
+  DEVICE_ADDRESS_TX_2 = 0x03
+};
+
+DeviceAddress THIS_DEVICE_ADDRESS = DEVICE_ADDRESS_RX;
+
+LoRaModule lora;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+TimerTask tim1(50);
+TimerTask tim2(100);
+TimerTask lcdTim(1000);
+
+DigitalOut ledMerah(7);
+DigitalOut ledKuning(6);
+DigitalOut ledHijau(5);
+DigitalOut buzzer(4);
+
+int data, status = 1, keyFrameAnim = 0;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Serial.print("Initialize...");
+  lcd.init();
+  lcd.backlight();
+
+  if (THIS_DEVICE_ADDRESS != DEVICE_ADDRESS_RX) proximitySetup();
+  lora.init(10, 9, 8);
+
+  Serial.println("Done..");
+}
+
+void loop() {
+  if (tim1.triggered()) lora.receive(onReceive);
+  if (tim2.triggered()) {
+    if (THIS_DEVICE_ADDRESS != DEVICE_ADDRESS_RX) sendData();
+  }
+  if (lcdTim.triggered()) lcdIdleAnimation();
+  
+  ledMerah.update();
+  ledKuning.update();
+  ledHijau.update();
+  buzzer.update();
+
+#ifdef DEBUG
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil("\n");
+    data = sendDataFromSerial(input);
     lora.clearData();
     lora.addData(THIS_DEVICE_ADDRESS);
     lora.addData(data);
     lora.addData(status);
-    for (int i = 0; i < 20; i++) lora.sendDataAsyncCb(1000, sendDataCallback);
+    lora.sendDataAsyncCb(1000, sendDataCallback);
   }
-}
-
-void sendDataCallback() {
-  ledKuning.on();
-  ledKuning.offDelay(200);
-
-#ifdef DEBUG
-  Serial.print("| NODE :");
-  Serial.print(THIS_DEVICE_ADDRESS);
-  Serial.print("| SEND: ");
-  Serial.print(data);
-  Serial.print("| ");
-  Serial.print(status);
-  Serial.println();
 #endif
-}
 
-
-int sendDataFromSerial(String input) {
-  input.trim();
-  return input.toInt();
-}
-void onReceive(String datas) {
-  int addressRecv = (int)lora.getData(datas, 0, ";");
-  int dataRecv = lora.getData(datas, 1, ";");
-  bool statusRecv = lora.getData(datas, 2, ";");
-
-  if (THIS_DEVICE_ADDRESS == DEVICE_ADDRESS_TX_1 || THIS_DEVICE_ADDRESS == DEVICE_ADDRESS_TX_2) {
-    if (addressRecv == DEVICE_ADDRESS_RX) {
-      ledHijau.on();
-      ledHijau.offDelay(200);
-
-      buzzer.on();
-      buzzer.offDelay(1000);
-
-      printReceivedData(addressRecv, dataRecv, statusRecv);
-    }
-  } else {
-    if (addressRecv == DEVICE_ADDRESS_TX_1) {
-      ledMerah.on();
-      ledMerah.offDelay(200);
-      printReceivedData(addressRecv, dataRecv, statusRecv);
-    }
-    if (addressRecv == DEVICE_ADDRESS_TX_2) {
-      ledHijau.on();
-      ledHijau.offDelay(200);
-      printReceivedData(addressRecv, dataRecv, statusRecv);
-    }
-  }
+  
 }
