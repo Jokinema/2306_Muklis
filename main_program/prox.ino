dacef03357eea772ae773a38a456c6a10e195b21
Dwi Setia Fardhana <84776462+CallmeSetia@users.noreply.github.com>
2024-05-02 14:23:36 +0700

    Update prox.ino

--- main_program/prox.ino
+++ main_program/prox.ino
@@ -4,12 +4,15 @@ void proximitySetup() {
 
 int getDataProx() {
   int data = analogRead(A7);
-
+  
 #ifdef DEBUG
   Serial.print("| NODE :");
   Serial.print(THIS_DEVICE_ADDRESS);
+  Serial.print(" | ");
   Serial.print("[SENSOR] PROX READ :");
-  Serial.println(data);
+  Serial.print(data);
+  Serial.print(" | Logic : ");
+  Serial.println(data < 512 ? 1 : 0);
 #endif
 
   return data < 512 ? 1 : 0;
